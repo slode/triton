5fb1b06d98698374f4c0ef1fea5aed68bbaa5921
Stian Lode <stian.lode@gmail.com>
2013-08-15 22:14:34 +0200

    Added fabric-like material demo.

--- linked_bodies.py
+++ linked_bodies.py
@@ -4,7 +4,7 @@ from triton.vector2d import Vector2d
 from triton.shape import Sphere, SpatialHash
 
 class SpringDamperLink:
-    def __init__(self, rb1, rb2, damping=1, spring=1, length=50):
+    def __init__(self, rb1, rb2, damping=.3, spring=4.0, length=50):
         self._rb1 = rb1
         self._rb2 = rb2
         self._damping = damping
@@ -33,86 +33,56 @@ def main():
     spheres = []
     sphere_col = []
     center = Vector2d(400.0, 400.0)
-    g_constant = 1.005
     downward = Vector2d(0.0, 9.81)
 
-    stationary_sphere =  Sphere(
-            mass = 10**23,
-            radius = 5.0,
-            pos = Vector2d(300.0, 30.0),
-            damping = 0.4,
-            elasticity = 0.97
-            )
-    spheres.append(stationary_sphere)
-    sphere_col.append((int(stationary_sphere.x)%255,int(stationary_sphere.y)%255,int(stationary_sphere.radius)*255%255))
+    width = 7
+    length = 6
+    for y in range(1, length+1):
+        for x in range(1, width+1):
+            pos = Vector2d(float(100.0*x), float(50.0*y))
+            sphere = Sphere(
+                mass = 0.3,
+                radius = 1.0,
+                pos = pos,
+                damping = 0.1,
+                elasticity = 0.97
+                )
 
-    stationary_sphere2=  Sphere(
-            mass = 10**23,
-            radius = 5.0,
-            pos = Vector2d(400.0, 30.0),
-            damping = 0.4,
-            elasticity = 0.97
-            )
-    spheres.append(stationary_sphere2)
-    sphere_col.append((int(stationary_sphere2.x)%255,int(stationary_sphere2.y)%255,int(stationary_sphere2.radius)*255%255))
+            spheres.append(sphere)
+            sphere_col.append((int(sphere.x)%255,int(sphere.y)%255,int(sphere.radius)*255%255))
 
-    stationary_sphere3=  Sphere(
-            mass = 10**23,
-            radius = 5.0,
-            pos = Vector2d(500.0, 30.0),
-            damping = 0.4,
-            elasticity = 0.97
-            )
-    spheres.append(stationary_sphere3)
-    sphere_col.append((int(stationary_sphere3.x)%255,int(stationary_sphere3.y)%255,int(stationary_sphere3.radius)*255%255))
-
-    for i in range(9):
-        pos = Vector2d(random.random()*800.0, random.random()*800.0)
-        vel = (center - pos).perp().normalize()
-        sphere = Sphere(
-            mass = 1.5,
-            radius = 1.0,
-            pos = pos,
-            damping = 0.99,
-            elasticity = 0.97
-            )
-
-        spheres.append(sphere)
-        sphere_col.append((int(sphere.x)%255,int(sphere.y)%255,int(sphere.radius)*255%255))
+    for sphere in spheres[-width:]:
+        sphere.mass = 10.0
 
     links = []
     for i, sphere in enumerate(spheres):
         try:
-            down = spheres[i+3]
+            down = spheres[i+width]
             links.append(SpringDamperLink(sphere, down, length=100))
         except:
             pass
 
         try:
-            if i % 3 > 0:
-                downright = spheres[i+2]
+            if i % width > 0:
+                downright = spheres[i+width-1]
                 links.append(SpringDamperLink(sphere, downright, length=140))
         except:
             pass
 
         try:
-            if (i+1) % 3 != 0:
+            if (i+1) % width != 0:
                 left = spheres[i+1]
                 links.append(SpringDamperLink(sphere, left, length=100))
         except:
             pass
 
         try:
-            if (i+1) % 3 != 0:
-                downleft = spheres[i+4]
+            if (i+1) % width != 0:
+                downleft = spheres[i+width+1]
                 links.append(SpringDamperLink(sphere, downleft, length=140))
         except:
             pass
 
-
-    for sphere in spheres[-3:]:
-        sphere.mass = 10.0
-    
     t = 0
     dt = 0.1
 
@@ -124,14 +94,7 @@ def main():
         grid.update(spheres)
 
         for sphere in spheres:
-            break
-            for neighbour in grid.nearby_objects(sphere):
-                if sphere.collides_with(neighbour):
-                    sphere.resolve_collision(neighbour)
-
-        for sphere in spheres:
-            if sphere != stationary_sphere and sphere != stationary_sphere2 and sphere != stationary_sphere3:
-                sphere.apply_force_to_com(sphere.mass * downward)
+            sphere.apply_force_to_com(sphere.mass * downward)
 
         for link in links:
             link.resolve()
@@ -144,7 +107,7 @@ def main():
             pygame.draw.circle(
                     screen, sphere_col[n], sphere.pos.tuple(), int(sphere.radius), 0)
 
-        for sphere in spheres:
+        for sphere in spheres[width:]:
             sphere.update(t, dt)
 
         pygame.display.flip()
