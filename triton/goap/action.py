b2832ce1d55e14aa26e1fdd7bf26472f22051cc8
Stian Lode <sl@rds.no>
2018-03-19 10:56:02 +0100

    Brute-forcy GOAP. Poor dwarves.

--- triton/goap/action.py
+++ triton/goap/action.py
@@ -4,14 +4,21 @@ class Action:
     effects = {}
     cost = 1.0
 
+    def __init__(self):
+        self.visits = 0
+
+    def visited(self):
+        self.visits += 1
+        return self.visits
+
     def is_done(self):
         return True
 
     def check_precondition(self):
         return True
 
-    def perform_action(self, agent):
-        print("Performing " + __class__ + ".")
+    def perform(self, agent):
+        print("Performing " + str(self) + ".")
 
     def __str__(self):
         return self.__repr__()
@@ -37,43 +44,66 @@ class Agent:
     def get_actions(self):
         raise NotImplementedError()
 
+    def do_action(self, action):
+        action.perform(self)
+        self.state.update(action.effects)
+
     def set_goal(self, goal):
-        self.goal.prepend(goal)
+        self.goals.insert(0, goal)
 
-    def get_goal(self):
-        return self.goal[0] if len(self.goal) else None
+    def get_goals(self):
+        return self.goals
 
-def goal_planner(agent, goal):
+    def verify_goals(self):
+        for g in self.goals:
+            if set(g.get_conditions().items()) <= set(self.state.items()):
+                self.goals.remove(g)
+        return len(self.goals) == 0
+
+def goal_planner(agent):
     actions = set(action() for action in agent.actions)
-    goal_conditions = dict(goal.get_conditions())
     state = dict(agent.state)
 
     def validate_actions(state, actions, goal_conditions, cost=0):
-        valid_actions = {}
+
         if set(goal_conditions.items()) <= set(state.items()):
             return cost
 
+        valid_actions = []
         for action in actions:
-            # can I perform this action now
-            newstate = state.copy()
-            newstate.update(action.effects)
             if set(action.preconditions.items()) <= set(state.items()):
-                valid_actions[action] = validate_actions(
-                        newstate,
+                new_state = state.copy()
+                new_state.update(action.effects)
+
+                total_cost = validate_actions(
+                        new_state,
                         actions - set([action]),
                         goal_conditions,
                         cost + action.cost)
 
-        return valid_actions
+                if total_cost is not None:
+                    valid_actions.append((total_cost, action))
 
-    v = validate_actions(state, actions, goal_conditions)
-    import pprint
-    pprint.pprint(v, width=10, depth=10)
+        if not valid_actions:
+            return None
 
+        valid_actions.sort()
+        return valid_actions[0]
+
+    v = None
+    for goal in agent.goals:
+        goal_conditions = dict(goal.get_conditions())
+        v = validate_actions(state, actions.copy(), goal_conditions)
+        if v is not None:
+            break
+
+    if v is not None:
+        return v[-1]
 
 if __name__ == "__main__":
+
     class StealOre(Action):
-        preconditions = {}
+        preconditions = {"hasMoney": False}
         effects = {"hasOre": True}
         cost = 20.0
 
@@ -96,7 +126,7 @@ if __name__ == "__main__":
 
     class BuyTool(Action):
         preconditions = {"hasTool": False, "hasMoney": True}
-        effects = {"hasTool": True}
+        effects = {"hasTool": True, "hasMoney": False}
 
     class Dwarf(Agent):
         def init(self):
@@ -109,6 +139,12 @@ if __name__ == "__main__":
             self.actions.append(Brawl)
 
     gimli = Dwarf()
-    tool_goal = Goal({"hasFun": True})
-    goal_planner(gimli, tool_goal)
+    gimli.set_goal(Goal({"hasTool": True}))
+    gimli.set_goal(Goal({"hasFun": True}))
 
+    while True:
+        if gimli.verify_goals():
+            print("Reached goal")
+            break
+        action = goal_planner(gimli)
+        gimli.do_action(action)
