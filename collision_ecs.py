c1a13f33482b9bc9a0739ad11f48088a57ba0d97
Stian Lode <stian.lode@canrig.com>
2019-04-02 15:09:26 +0200

    Removed whitespace

--- collision_ecs.py
+++ collision_ecs.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2013 Stian Lode
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,8 +17,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
- 
- 
+
+
 from triton.vector2d import Vector2d
 from triton.sphere import Sphere
 from triton.spatial_hash import SpatialHash
@@ -37,7 +37,6 @@ class Centroid(Component):
     def new_c(self):
         self.center = Vector2d(random.randrange(800.0),
                                random.randrange(800.0))
-
 
 class RigidBody(Component):
     def __init__(self, sphere):
@@ -87,7 +86,7 @@ class RenderSystem(System):
         self.clock = pygame.time.Clock()
         self.draw = None
         self.toggle_draw()
-    
+
     def toggle_draw(self):
         self.draw = (pygame.gfxdraw.aacircle
                 if self.draw == pygame.gfxdraw.filled_circle
