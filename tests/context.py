cf52d8763009c15b27419e7c1cce96a3bb643cee
Stian Lode <stian.lode@canrig.com>
2021-10-26 15:07:17 +0200

    emoves

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-import os
-import pytest
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-@pytest.fixture
-def client():
-    client = None
-    yield client
